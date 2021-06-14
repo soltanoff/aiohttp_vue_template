15293649eea16de6c8cd83a14d74b74699e6903d
soltanoff <piccadillable@gmail.com>
2021-06-14 14:43:23 +0300

    upd index.py

--- routers/index.py
+++ routers/index.py
@@ -1,12 +1,13 @@
 import aiohttp_jinja2
+from aiohttp import web
 
 import settings
 
 
-def setup_routes(app):
-    app.router.add_get('/', index)
+router = web.RouteTableDef()
 
 
+@router.get('/')
 @aiohttp_jinja2.template(settings.BASE_TEMPLATE_NAME)
 async def index(request):
     return {'request': request, 'csrf_token': 'TODO'}
